92988b922560adc1102833d9d776e05e08be3ed2
Neeraj Vijayvargiya <neeraj.vijayvargia@pinelabs.com>
2023-03-22 18:25:47 +0530

    firebase added

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: 'AIzaSyA7TblX1JMM3s4OD_KkBRqlWPtM7XGg1-s',
+    authDomain: 'task-management-ca778.firebaseapp.com',
+    projectId: 'task-management-ca778',
+    storageBucket: 'task-management-ca778.appspot.com',
+    messagingSenderId: '1061722305850',
+    appId: '1:1061722305850:web:88c00665a81fb41ae34c03',
+    measurementId: 'G-VY2Q84LDD5',
+  }
